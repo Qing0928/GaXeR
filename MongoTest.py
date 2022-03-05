7eba3cee660d2613d2e382e7e4bfefc4ae03eda0
Qing0928 <73458231+Qing0928@users.noreply.github.com>
2022-03-05 17:17:55 +0800

    2022/03/05

--- MongoTest.py
+++ MongoTest.py
@@ -55,7 +55,61 @@ result = mongocollection.update_one(
     }
 )
 #print(result)
-result = list(mongocollection.find({"account":"test01"}, {"_id":0, "profile":0}).limit(3))
+'''result = list(mongocollection.find(
+    {"profile.token":"123456abcd"}, 
+    {"_id":0, "profile":0, "account":0, "gas1.battery":0, "gas1.uswitch":0}
+    ).limit(1))
 pprint(result)
+result = list(mongocollection.find(
+    {"gas1.data.time":{
+        "$gt":1646405285
+    }},  
+    {"_id":0, "profile":0, "account":0, "gas1.battery":0, "gas1.uswitch":0,}))
+pprint(result)
+mongocollection.update_one(
+    {"account":"test01"}, 
+    {"$push":{
+        "gas1.data":{
+            "time":1646469163, 
+            "fire":1689949, 
+            "temp":29.8, 
+            "gas":33, 
+            "remaining":1940.63
+            }}}
+)
+result = list(mongocollection.aggregate([
+    {"$match":{
+        "account":"test02"}
+        }, 
+    {"$unwind":"$gas1.data"},
+    {"$match":{
+        "gas1.data.temp":{"$gt":29}}
+        }, 
+    {"$project":{
+        "gas1.data":1, "_id":0}
+        }, 
+    {"$limit":5}
+    ])
+    )
+print(result)
+'''
+mongocollection.update_many(
+    {"account":"test01"},
+    {
+        "$push":{
+                "gas1.data":{
+                "time":1646471042, 
+                "fire":1689949, 
+                "temp":29.8, 
+                "gas":29, 
+                "remaining":1948
+                }
+            }, 
+        "$set":{
+            "gas1.uswitch":"False", 
+            "gas1.battery":89
+            }   
+    }, upsert=True
+)
 #print(type(result))
 mongoclient.close()
